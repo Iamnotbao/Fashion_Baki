71a6f5d4454aca0da7ad5eab8e519fd4768fe27f
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2025-05-07 00:12:29 +0700

    update

--- client/src/services/recommendationServices.jsx
+++ client/src/services/recommendationServices.jsx
@@ -1,11 +1,11 @@
 import axios from "axios"
 
-const BASE_URL = "/api/recommendation"
+const BASE_URL = import.meta.env.VITE_API_URL
 export const GetAllRecommendation= async(id)=>{
     try {
         console.log("id: ",id);
         
-        const response = await axios.get(`${BASE_URL}/${id}`);
+        const response = await axios.get(`${BASE_URL}/recommendation/${id}`);
         console.log("recommend response for produtcs: ", response.data);
         return response.data ? response.data : null;
         
