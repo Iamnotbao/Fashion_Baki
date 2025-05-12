c01cff839e0e073700b10090ce284daebccaeaea
Iamnotbao <ohshit30102015@gmail.com>
2025-05-12 08:34:19 +0700

    update for all

--- client/src/services/orderServices.jsx
+++ client/src/services/orderServices.jsx
@@ -1,25 +1,25 @@
 import axios from  'axios';
 
 
-const API_BASE_URL = '/api/service/orders';
+const API_BASE_URL = import.meta.env.VITE_API_URL ;
 
 
 export const getAllOrder =async()=>{
-    const response = await axios.get(API_BASE_URL);
+    const response = await axios.get(`${API_BASE_URL}/service/orders`);
     return response.data;
 }
  
 export const chekOutOrder =async(item)=>{ 
-    const response = await axios.post(`${API_BASE_URL}/checkout`,item);
+    const response = await axios.post(`${API_BASE_URL}/service/orders/checkout`,item);
     return response.data;
 }
 export const getOrderById =async(orderId )=>{
     
-    const response = await axios.get(`${API_BASE_URL}/${orderId }`)
+    const response = await axios.get(`${API_BASE_URL}/service/orders/${orderId }`)
     return response.data;
 }
 export const getAllOrderByUser =async()=>{
-    const response = await axios.get(`${API_BASE_URL}/user`);
+    const response = await axios.get(`${API_BASE_URL}/service/orders/user`);
     return response.data;
 }
 
