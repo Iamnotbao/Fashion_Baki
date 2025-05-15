a73eb07809e77e2d3b15c6211e71c1dec61400ff
Iamnotbao <146004881+Iamnotbao@users.noreply.github.com>
2025-05-15 15:29:39 +0700

    update

--- client/src/sections/HomeCategory/HomeCategory.jsx
+++ client/src/sections/HomeCategory/HomeCategory.jsx
@@ -21,8 +21,8 @@ const HomeCategory = ({ eachBanner }) => {
     console.log("auto select", selectedProduct);
     console.log(eachBanner);
     const url = import.meta.env.VITE_API_URL;
- 
-    
+
+
     useEffect(() => {
         const fetch = async () => {
             try {
@@ -86,6 +86,18 @@ const HomeCategory = ({ eachBanner }) => {
             },
         });
     }
+    const isOutOfStock = (product) => {
+        const selections = selectedProduct[product.id] || {};
+        const selectedSize = selections.size || product.sizes?.[0];
+        const selectedColor = selections.color || product.colors?.[0];
+
+        const stockItem = product.stocks?.find(
+            (item) => item.size === selectedSize && item.color === selectedColor
+        );
+        console.log("stockItem", stockItem);
+        
+        return !stockItem || stockItem.quantity < 1;
+    };
 
     return (
         <section className="home-product">
@@ -106,105 +118,113 @@ const HomeCategory = ({ eachBanner }) => {
                                             <a href={`/category/${cat.id}`} className="view-all">View all</a>
                                         </div>
                                     </div>
-                                        {productsByCat[cat.id]?.length > 0 ? (
-                                            <Swiper
-                                                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                                                spaceBetween={10}
-                                                slidesPerView={5}
-                                                loop={true}
-                                                navigation
-                                                pagination={{
-                                                    clickable: true,
-                                                    type: 'fraction'
-                                                }}
-                                                scrollbar={{ draggable: true }}
-                                                onSwiper={(swiper) => {
-                                                    swiperRef.current = swiper;
-                                                    swiper.update();
+                                    {productsByCat[cat.id]?.length > 0 ? (
+                                        <Swiper
+                                            modules={[Navigation, Pagination, Scrollbar, A11y]}
+                                            spaceBetween={10}
+                                            slidesPerView={5}
+                                            loop={true}
+                                            navigation
+                                            pagination={{
+                                                clickable: true,
+                                                type: 'fraction'
+                                            }}
+                                            scrollbar={{ draggable: true }}
+                                            onSwiper={(swiper) => {
+                                                swiperRef.current = swiper;
+                                                swiper.update();
 
-                                                }}
-                                                style={{ width: '80%', margin: '0 auto', marginTop: "0" }}
-                                            >
-                                                {productsByCat[cat.id].map((product) => (
-                                                    <SwiperSlide key={product.id}>
-                                                        <div className="product-card">
-                                                            <div className="product-img" onClick={() => handleBuy(product.id)}>
-                                                                <img src={product.image} alt={product.name} />
-                                                            </div>
-                                                            <div className="product-content">
-                                                                <div className="product-colors" style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
-                                                                    {product.colors.map((color) => (
-                                                                        <div
-                                                                            key={`${product.id}-${color}`}
-                                                                            className="product-color-choose"
+                                            }}
+                                            style={{ width: '80%', margin: '0 auto', marginTop: "0" }}
+                                        >
+                                            {productsByCat[cat.id].map((product) => (
+                                                <SwiperSlide key={product.id}>
+                                                    <div className="product-card">
+                                                        <div className="product-img" onClick={() => handleBuy(product.id)}>
+                                                            <img src={product.image} alt={product.name} />
+                                                        </div>
+                                                        <div className="product-content">
+                                                            <div className="product-colors" style={{ display: "flex", flexDirection: "row", gap: "5px" }}>
+                                                                {product.colors.map((color) => (
+                                                                    <div
+                                                                        key={`${product.id}-${color}`}
+                                                                        className="product-color-choose"
+                                                                        style={{
+                                                                            width: "45px",
+                                                                            height: "24px",
+                                                                            borderRadius: "16px",
+                                                                            border: (selectedProduct?.[product.id]?.color || product.colors[0]) === color
+                                                                                ? "1px solid #686868"
+                                                                                : "1px solid #fff",
+                                                                            position: "relative"
+                                                                        }}
+                                                                    >
+                                                                        <input
+                                                                            type="radio"
+                                                                            name="color"
+                                                                            value={color}
+                                                                            checked={selectedProduct[product.id]?.color === color}
+                                                                            onChange={(e) => handleChangeSizeOrColor(e, product.id)}
+                                                                            id={`${product.id}-${color}`}
                                                                             style={{
-                                                                                width: "45px",
-                                                                                height: "24px",
+                                                                                position: "absolute",
+                                                                                width: "100%",
+                                                                                height: "100%",
+                                                                                opacity: 0
+                                                                            }}
+                                                                        />
+                                                                        <label
+                                                                            htmlFor={`${product.id}-${color}`}
+                                                                            className="product-color"
+                                                                            style={{
+                                                                                cursor: "pointer",
+                                                                                backgroundColor: color,
+                                                                                width: "39px",
+                                                                                height: "17px",
                                                                                 borderRadius: "16px",
-                                                                                border: (selectedProduct?.[product.id]?.color || product.colors[0]) === color
-                                                                                    ? "1px solid #686868"
-                                                                                    : "1px solid #fff",
-                                                                                position: "relative"
+                                                                                display: "inline-block",
+                                                                                marginBottom: 0,
+                                                                                position: "absolute",
+                                                                                top: "50%",
+                                                                                left: "50%",
+                                                                                transform: "translate(-50%, -50%)"
                                                                             }}
                                                                         >
-                                                                            <input
-                                                                                type="radio"
-                                                                                name="color"
-                                                                                value={color}
-                                                                                checked={selectedProduct[product.id]?.color === color}
-                                                                                onChange={(e) => handleChangeSizeOrColor(e, product.id)}
-                                                                                id={`${product.id}-${color}`}
-                                                                                style={{
-                                                                                    position: "absolute",
-                                                                                    width: "100%",
-                                                                                    height: "100%",
-                                                                                    opacity: 0
-                                                                                }}
-                                                                            />
-                                                                            <label
-                                                                                htmlFor={`${product.id}-${color}`}
-                                                                                className="product-color"
-                                                                                style={{
-                                                                                    cursor: "pointer",
-                                                                                    backgroundColor: color,
-                                                                                    width: "39px",
-                                                                                    height: "17px",
-                                                                                    borderRadius: "16px",
-                                                                                    display: "inline-block",
-                                                                                    marginBottom: 0,
-                                                                                    position: "absolute",
-                                                                                    top: "50%",
-                                                                                    left: "50%",
-                                                                                    transform: "translate(-50%, -50%)"
-                                                                                }}
-                                                                            >
-                                                                            </label>
-                                                                        </div>
-                                                                    ))}
-                                                                </div>
-                                                                <p className="product-title" onClick={() => handleBuy(product.id)}>{product.name}</p>
-                                                                <div className="product-price-tag">
-                                                                    <p>{product.price}$</p>
-                                                                    <p>{product.rating}<span><i className="fa-solid fa-star"></i></span></p>
-                                                                </div>
-                                                                <div>{product.status}</div>
+                                                                        </label>
+                                                                    </div>
+                                                                ))}
                                                             </div>
+                                                            <p className="product-title" onClick={() => handleBuy(product.id)}>{product.name}</p>
+                                                            <div className="product-price-tag">
+                                                                <p>{product.price}$</p>
+                                                                <p>{product.rating}<span><i className="fa-solid fa-star"></i></span></p>
+                                                            </div>
+                                                            <div>{product.status}</div>
                                                         </div>
-                                                        <Sizes handleOption={handleChangeSizeOrColor} setSelectedSize={setSelectedProduct} selectedProduct={selectedProduct} product={product} />
-                                                        <button className="addToCart" onClick={() => handleCart(product)}>Add to cart</button>
-                                                    </SwiperSlide>
-                                                ))}
+                                                    </div>
+                                                    <Sizes handleOption={handleChangeSizeOrColor} setSelectedSize={setSelectedProduct} selectedProduct={selectedProduct} product={product} />
+                                                    {isOutOfStock(product) ? (
+                                                        <button className="outOfCart" disabled style={{ backgroundColor: '#ccc', cursor: 'not-allowed' }}>
+                                                            Out of Stock
+                                                        </button>
+                                                    ) : (
+                                                        <button className="addToCart" onClick={() => handleCart(product)}>
+                                                            Add to Cart
+                                                        </button>
+                                                    )}
+                                                </SwiperSlide>
+                                            ))}
 
-                                            </Swiper>
-                                        ) : (
-                                            <p>Loading products for {cat.name}...</p>
-                                        )
+                                        </Swiper>
+                                    ) : (
+                                        <p>Loading products for {cat.name}...</p>
+                                    )
 
-                                        }
-                                    </div>
-                                </div></div>
-                        </div>
-                    
+                                    }
+                                </div>
+                            </div></div>
+                    </div>
+
                 </>
             ))}
         </section>
